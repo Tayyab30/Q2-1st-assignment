a386a1642c2a00140467c20243ed95cbc818379a
Tayyab30 <mtayyabjaved30@gmail.com>
2023-11-20 12:52:41 +0500

    fix: home page design.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+// import './globals.css'
+import './style.css'
+import '@fortawesome/fontawesome-free/css/all.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
